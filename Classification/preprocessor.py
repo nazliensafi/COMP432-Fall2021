ae75ab493791dae467d23b8856b6309cd9e44f50
ginibrandon <gini@forsaw.ca>
2021-12-04 12:07:40 -0500

    more debugging

--- Classification/preprocessor.py
+++ Classification/preprocessor.py
@@ -10,11 +10,12 @@
 import numpy as np
 import sklearn as sk
 from sklearn import linear_model, tree, svm, ensemble, neighbors, naive_bayes, neural_network
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from scipy.io.arff import loadarff
 import pandas as pd
+import arff
 
 
 def load_dataset(dataset, file_loc): # passingdataset_details instead of dataset
@@ -57,24 +58,28 @@ def create_encoders(X, y):
     '''
 
     cat_enc = OneHotEncoder(handle_unknown='ignore')
-    num_enc = StandardScaler()
+    num_enc = MinMaxScaler()
 
-    cat_features = X.select_dtypes(include=['object']).columns.astype(str)
-    num_features =X.select_dtypes(include=['int64', 'float64']).columns.astype(str)
+    cat_features = X.select_dtypes(include=['object']).columns
+    num_features =X.select_dtypes(include=['int64', 'float64']).columns
 
-    X_enc = ColumnTransformer(
-        transformers=[
-            ("num", num_enc, num_features),
-            ("cat", cat_enc, cat_features)
-        ]
-    )
+    if len(cat_features)==0:
+        X_enc = num_enc
+    elif len(num_features)==0:
+        X_enc = cat_enc
+    else:
+        X_enc = ColumnTransformer(
+            transformers=[
+                ("num", num_enc, num_features),
+                ("cat", cat_enc, cat_features)
+            ]
+        )
     
     y_enc = None
 
     if y.dtypes=='object':
         y_enc = LabelEncoder()
 
-    print(X_enc, y_enc)
     return X_enc, y_enc
 
 
@@ -89,8 +94,11 @@ def preprocessor(dataset_details, file_loc):
     train_data = {}
     test_data = {}
     for dataset in dataset_details:
-        X, y = load_dataset(dataset_details[dataset], file_loc) #this is being done in get_X_and_y(dataset_details): line 119
+        X, y = load_dataset(dataset_details[dataset], file_loc)
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+        if dataset=='breast_cancer': 
+            X_train = np.nan_to_num(X,nan=10)
+            X_test = np.nan_to_num(X,nan=10)
         train_data[dataset] = {
             'X_train': X_train,
             'y_train': y_train
@@ -100,7 +108,6 @@ def preprocessor(dataset_details, file_loc):
             'y_test': y_test
         }
 
-    print("Loaded data: ",train_data.keys, test_data.keys)
     return train_data, test_data
 
 
@@ -110,16 +117,15 @@ def load_excel(file,  **kwargs):
 
 
 def load_arff(file):
-    df = loadarff(file)
-    df = pd.DataFrame(df, dtype=None)
-
+    data = loadarff(file)
+    df = pd.DataFrame(data[0])
+    df.dropna()
     return df
 
 
 def load_plaintext(file, **kwargs):
     df = pd.read_csv(file, header=None, dtype=None, **kwargs)
     df.dropna()
-
     return df
 
 
@@ -146,17 +152,21 @@ def train_clf(clf_data, X, y):
     '''
     Trains a given classifier on a given dataset
     '''
-    print(X,y)
+    
     X_enc, y_enc = create_encoders(X, y)
-    X = X_enc.fit(X)
+    X_enc_model = X_enc.fit(X)
+    X = X_enc_model.transform(X)
+    if isinstance(X,pd.DataFrame):
+        X = X.toarray()
+
     if y_enc:
         y = y_enc.fit_transform(y)
 
+
     clf = clf_data['clf']
     params = clf_data['params']
-
     model = clf(**params).fit(X,y)
-
+    
     return model
 
 
@@ -166,6 +176,14 @@ def main():
 
 
     dataset_details = {
+        # 'credit':
+        #     {
+        #         'file': ['default of credit card clients.xls'],
+        #         'load_params': {
+        #             'index_col': 0,
+        #             'skiprows': 1
+        #         }
+        #     },
         'breast_cancer':
             {
                 'file': ['breast-cancer-wisconsin.data'],
@@ -174,65 +192,45 @@ def main():
                     'index_col': 0
                 },
                 'weighted': 'Y'
+            },
+        # 'statlog':
+        #     {
+        #         'file': ['german.data-numeric'],
+        #         'load_params': {
+        #             'delim_whitespace': 'true'
+        #         },
+        #         'weighted': 'Y',
+        #     },
+        # 'adult':
+        #     {
+        #         'file': ['adult.data','adult.test'],
+        #         'load_params': {
+        #             'na_values': '?',
+        #             'comment': '|'
+        #         }
+        #     },
+        'yeast': {
+            'file': ['yeast.data'],
+            'load_params': {
+                'index_col': 0,
+                'delim_whitespace': 'true'
             }
+        }
+        # 'thoracic': {
+        #     'file': ['ThoraricSurgery.arff'],
+        #     'load_params': {}
+        # },
+        # 'seismic': {
+        #     'file': ['seismic-bumps.arff'],
+        #     'load_params': {}
+        # },
+        # 'retinopathy': {
+        #     'file': ['messidor_features.arff'],
+        #     'load_params': {
+        #         'comment': '@'
+        #     }
+        # }
     }
-
-    # dataset_details = {
-    #     'credit':
-    #         {
-    #             'file': ['default of credit card clients.xls'],
-    #             'load_params': {
-    #                 'index_col': 0,
-    #                 'skiprows': 1
-    #             }
-    #         },
-    #     'breast_cancer':
-    #         {
-    #             'file': ['breast-cancer-wisconsin.data'],
-    #             'load_params': {
-    #                 'na_values': '?',
-    #                 'index_col': 0
-    #             },
-    #             'weighted': 'Y'
-    #         },
-    #     'statlog':
-    #         {
-    #             'file': ['german.data-numeric'],
-    #             'load_params': {
-    #                 'delim_whitespace': 'true'
-    #             },
-    #             'weighted': 'Y',
-    #         },
-    #     'adult':
-    #         {
-    #             'file': ['adult.data','adult.test'],
-    #             'load_params': {
-    #                 'na_values': '?',
-    #                 'comment': '|'
-    #             }
-    #         },
-    #     'yeast': {
-    #         'file': ['yeast.data'],
-    #         'load_params': {
-    #             'index_col': 0,
-    #             'delim_whitespace': 'true'
-    #         }
-    #     },
-    #     'thoracic': {
-    #         'file': ['ThoraricSurgery.arff'],
-    #         'load_params': {}
-    #     },
-    #     'seismic': {
-    #         'file': ['seismic-bumps.arff'],
-    #         'load_params': {}
-    #     },
-    #     'retinopathy': {
-    #         'file': ['messidor_features.arff'],
-    #         'load_params': {
-    #             'comment': '@'
-    #         }
-    #     }
-    # }
 
     # Classifier metadata, including classifier and hyperparameters once chosen
     CLASSIFIERS = {
@@ -240,16 +238,8 @@ def main():
             'clf': linear_model.LogisticRegression,
             'params': {}
         },
-        'svc': {
-            'clf': svm.SVC,
-            'params': {}
-        },
         'tree': {
             'clf': tree.DecisionTreeClassifier,
-            'params': {}
-        },
-        'forest': {
-            'clf': ensemble.RandomForestClassifier,
             'params': {}
         },
         'kneighbors': {
@@ -264,8 +254,16 @@ def main():
             'clf': naive_bayes.GaussianNB,
             'params': {}
         },
+        'forest': {
+            'clf': ensemble.RandomForestClassifier,
+            'params': {}
+        },
         'neural': {
             'clf': neural_network.MLPClassifier,
+            'params': {}
+        },
+        'svc': {
+            'clf': svm.SVC,
             'params': {}
         }
     }
