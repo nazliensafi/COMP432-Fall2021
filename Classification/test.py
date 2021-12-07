a366ba3ce95a801e822be073de6a8897f0c84d23
ginibrandon <gini@forsaw.ca>
2021-12-06 22:00:19 -0500

    debugging

--- Classification/test.py
+++ Classification/test.py
@@ -6,11 +6,12 @@ import sklearn as sk
 from sklearn import linear_model, tree, svm, ensemble, neighbors, naive_bayes, neural_network
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split, GridSearchCV, RepeatedStratifiedKFold
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 from sklearn.impute import SimpleImputer
 from scipy.io.arff import loadarff
 import pandas as pd
 import scipy
+import matplotlib.pyplot as plt
 ### Data and Classifier Parameters  
 #Relative path root for datasets
 file_loc = os.path.join(os.getcwd(),'Classification/Datasets/')
@@ -42,14 +43,14 @@ dataset_details = {
             },
             'weighted': 'Y',
         },
-    'adult':
-        {
-            'file': ['adult.data','adult.test'],
-            'load_params': {
-                'na_values': '?',
-                'comment': '|'
-            }
-        },
+    # 'adult':
+    #     {
+    #         'file': ['adult.data','adult.test'],
+    #         'load_params': {
+    #             'na_values': '?',
+    #             'comment': '|'
+    #         }
+    #     },
     'yeast': {
         'file': ['yeast.data'],
         'load_params': {
@@ -76,57 +77,20 @@ dataset_details = {
 # Classifiers used - params will be filled in with the chosen hyperparameters
 # Classifiers used and their subset of hyperparameters chosen to test with gridsearch
 CLASSIFIERS = {
-    'logreg': {
-        'clf': linear_model.LogisticRegression,
+    'neural': {
+        'clf': neural_network.MLPClassifier,
         'param_grid': {
-            'penalty' : ['l1', 'l2'],
-            'C' : [np.logspace(-4, 4, 20)]
+            'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
+            'activation': ['tanh', 'relu'],
+            'max_iter': [500]
         },
         'params': {
-            'random_state': 0
+            'max_iter': 500
         }
-    },
-    'tree': {
-        'clf': tree.DecisionTreeClassifier,
-        'param_grid': {
-            'criterion': ['gini','entropy'],
-            'min_samples_split': [5,10,50,100],
-            'min_samples_leaf': [1, 2, 4],
-            'max_depth': [1,5,10,50,100,None]
-        },
-        'params': {
-            'random_state': 0
-        }
-    },
-    'kneighbors': {
-        'clf': neighbors.KNeighborsClassifier,
-        'param_grid': {
-            'leaf_size': list(range(1,50)),
-            'n_neighbors': list(range(1,30)),
-            'p': [1,2]
-        },
-        'params': {}
-    },
-    'adaboost': {
-        'clf': ensemble.AdaBoostClassifier,
-        'param_grid': {
-            'n_estimators': [10, 50, 100, 500],
-            'learning_rate': [0.0001, 0.001, 0.01, 0.1, 1.0]
-            },
-        'params': {
-            'random_state': 0
-        }
-    },
-    'nb': {
-        'clf': naive_bayes.GaussianNB,
-        'param_grid': {
-            'var_smoothing': [np.logspace(0,-9, num=100)]
-        },        
-        'params': {}
     }
 }
 ### Preprocessing the data
-def preprocessor(dataset_details, file_loc):
+def preprocessor(dataset_details, file_loc, strategy='mean', encoder=StandardScaler, gridsearchflag=False):
 
     '''
     Loads all datasets in
@@ -138,23 +102,82 @@ def preprocessor(dataset_details, file_loc):
     test_data = {}
 
     for dataset in dataset_details:
+        if (gridsearchflag==True):
+            if (dataset!='yeast'):
+                continue
         X, y = load_dataset(dataset_details[dataset], file_loc)
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-        if dataset=='breast_cancer': #train_test_split is converting all 10.0 values to NaN for this dataset for some reason
-            X_train = X_train.fillna(value=10)
-            X_test = X_test.fillna(value=10)
+
+        #Encode categorical values and scale numeric values
+        X_enc, y_enc = create_encoders(X_train, y_train, encoder)
+        X_train = X_enc.transform(X_train)
+        X_test = X_enc.transform(X_test)
+        
+        if scipy.sparse.issparse(X_train):
+            X_train = X_train.toarray()
+            X_test = X_test.toarray()
+        if y_enc:
+            y_train = y_enc.transform(y_train)
+            y_test = y_enc.transform(y_test)
+
+        X_imp, X_train, X_test = impute(X_train, X_test, strategy)
 
         train_data[dataset] = {
             'X_train': X_train,
-            'y_train': y_train
+            'y_train': y_train,
         }
         test_data[dataset] = {
             'X_test': X_test,
-            'y_test': y_test
+            'y_test': y_test,
+            'X_imp': X_imp,
+            'X_enc': X_enc,
+            'y_enc': y_enc
         }
 
     return train_data, test_data
 
+def impute(X_train, X_test, strategy='mean'):
+    imp = SimpleImputer(missing_values=np.nan, strategy=strategy)
+    X_imp = imp.fit(X_train)
+    X_train = X_imp.transform(X_train)
+    X_test = X_imp.transform(X_test)
+
+    return X_imp, X_train, X_test
+
+def create_encoders(X, y, encoder):
+
+    '''
+    Splits dataset into numerical and categorical data
+    creates relevant encoders for both features and labels
+    '''
+
+    cat_enc = OneHotEncoder(handle_unknown='ignore')
+    num_enc = encoder()
+
+    cat_features = X.select_dtypes(include=['object']).columns
+    num_features =X.select_dtypes(include=['int64', 'float64']).columns
+
+    if len(cat_features)==0:
+        X_enc = num_enc
+    elif len(num_features)==0:
+        X_enc = cat_enc
+    else:
+        X_enc = ColumnTransformer(
+            transformers=[
+                ("num", num_enc, num_features),
+                ("cat", cat_enc, cat_features)
+            ]
+        )
+
+    X_enc.fit(X)
+    
+    y_enc = None
+    if y.dtypes=='object':
+        y_enc = LabelEncoder().fit(y)
+
+
+    return X_enc, y_enc
+    
 def load_dataset(dataset, file_loc):
     '''
     Loads in a dataset according to type and load_params
@@ -163,7 +186,7 @@ def load_dataset(dataset, file_loc):
     Separates out the last column as y
     '''
 
-    #metadata = dataset_details[dataset] # using dataset_details directly rather than metadata``
+    #metadata = dataset_details[dataset]
     filenames = dataset['file']
     load_params = dataset['load_params']
 
@@ -192,12 +215,10 @@ def load_excel(file,  **kwargs):
 def load_arff(file):
     data = loadarff(file)
     df = pd.DataFrame(data[0])
-    df.dropna()
     return df
 
 def load_plaintext(file, **kwargs):
     df = pd.read_csv(file, header=None, dtype=None, **kwargs)
-    df.dropna()
     return df
 
 ### Train Data
@@ -212,12 +233,9 @@ def train_classifiers(data, CLASSIFIERS):
         models[clf]={}
         for dataset in data:
             print("Training ",clf," on ",dataset)
-            model, X_enc, y_enc, X_imp = train_clf(CLASSIFIERS[clf], data[dataset]['X_train'], data[dataset]['y_train'])
+            model = train_clf(CLASSIFIERS[clf], data[dataset]['X_train'], data[dataset]['y_train'])
             models[clf][dataset] = {
-                'model': model,
-                'X_enc': X_enc,
-                'y_enc': y_enc,
-                'X_imp': X_imp
+                'model': model
             }
         
 
@@ -228,56 +246,11 @@ def train_clf(clf_data, X, y):
     '''
     Trains a given classifier on a given dataset
     '''
-    X_enc, y_enc = encode_or_scale(X, y)
-    X = X_enc.transform(X)
-    
-    if scipy.sparse.issparse(X):
-        X = X.toarray()
-    if y_enc:
-        y = y_enc.transform(y)
-
-    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-    X_imp = imp.fit(X)
-    X = X_imp.transform(X)
     params = clf_data['params']
     clf = clf_data['clf']
     model = clf(**params).fit(X,y)
     
-    return model, X_enc, y_enc, X_imp
-
-def encode_or_scale(X, y):
-
-    '''
-    Splits dataset into numerical and categorical data
-    creates relevant encoders for both features and labels
-    '''
-
-    cat_enc = OneHotEncoder(handle_unknown='ignore')
-    num_enc = StandardScaler()
-
-    cat_features = X.select_dtypes(include=['object']).columns
-    num_features =X.select_dtypes(include=['int64', 'float64']).columns
-
-    if len(cat_features)==0:
-        X_enc = num_enc
-    elif len(num_features)==0:
-        X_enc = cat_enc
-    else:
-        X_enc = ColumnTransformer(
-            transformers=[
-                ("num", num_enc, num_features),
-                ("cat", cat_enc, cat_features)
-            ]
-        )
-
-    X_enc.fit(X)
-    
-    y_enc = None
-    if y.dtypes=='object':
-        y_enc = LabelEncoder().fit(y)
-
-
-    return X_enc, y_enc
+    return model
     
 ### Test Models
 def test_classifiers(data, models):
@@ -297,30 +270,39 @@ def test_clf(models, X, y):
     print(model)
     print(model.score(X,y))
     
-def find_hyperparams(model, classifier_details, X, y):
-
+def find_hyperparams(classifier_details, X, y, gridsearch):
+    params = classifier_details['params']
+    model = classifier_details['clf'](**params)
     param_grid = classifier_details['param_grid']
-    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=0)
-    search = GridSearchCV(model, param_grid, cv=cv, n_jobs=-1)
-
+    cv = KFold(n_splits=5)
+    search = gridsearch(model, param_grid, cv=cv, n_jobs=-1)
     result = search.fit(X, y)
+    print (result.best_params_)
 
-    return result.best_params_
-
-def find_all_hyperparams(data, models, classifiers):
-    for clf in models:
-        for dataset in models[clf]:
-            hps = find_hyperparams(models[clf][dataset], classifiers[clf], data[dataset]['X_train'], data[dataset]['y_train'])
-            classifiers[clf]['final_params'] = hps
-
-
-train_data, test_data = preprocessor(dataset_details, file_loc)
-models = train_classifiers(train_data, CLASSIFIERS)
-find_all_hyperparams(train_data,models,CLASSIFIERS)
-test_classifiers(test_data, models)
+def find_all_hyperparams(data, classifiers, datasets, gridsearch=GridSearchCV, gridsearchflag=False):
+    count = 0
+    for clf in classifiers:
+        for dataset in datasets:
+            if (gridsearchflag==True):
+                if (dataset!='yeast'): continue
+            find_hyperparams(classifiers[clf], data[dataset]['X_train'], data[dataset]['y_train'], gridsearch)
+        count+=1
 
 
 
+#train_data, test_data = preprocessor(dataset_details, file_loc)
+#models = train_classifiers(train_data, CLASSIFIERS)
+#find_all_hyperparams(train_data,CLASSIFIERS, dataset_details)
+#test_classifiers(test_data, models)
+
+print("logreg",sum([0.81,0.97,0.75,0.57,0.60,0.82,0.94,0.67])/8)
+print("tree",sum([0.82,0.96,0.72,0.57,0.59,0.82,0.94,0.63])/8)
+print("kneighbors",sum([0.81,0.97,0.72,0.53,0.58,0.82,0.95,0.62])/8)
+print("adaboost",sum([0.83,0.96,0.74,0.54,0.44,0.82,0.95,0.66])/8)
+print("forest",sum([0.82,0.97,0.76,0.58,0.63,0.82,0.95,0.69])/8)
+print("nb",sum([0.64,0.95,0.69,0.19,0.11,0.19,0.42,0.61])/8)
+print("svc",sum([0.82,0.96,0.75,0.57,0.61,0.82,0.95,0.66])/8)
+print("neural",sum([0.81,0.97,0.75,0.52,0.60,0.80,0.94,0.70])/8)
 
 
 
